de47347ddd3a6f30995c05f4af3b0f641d44a5f8
GarbageCode1984 <rkqlwl1984@gmail.com>
2023-04-23 19:48:29 +0900

    Modal

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
 import "./Row.css";
+import MovieModal from "./MovieModal";
 
 export default function Row({ title, id, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [movieSelected, setMovieSelected] = useState({});
 
     useEffect(() => {
         fetchMovieData();
     }, []);
 
-    async function fetchMovieData() {
+    const fetchMovieData = async () => {
         const request = await axios.get(fetchUrl);
         setMovies(request.data.results);
-    }
+    };
+
+    const handleClick = (movie) => {
+        setModalOpen(true);
+        setMovieSelected(movie);
+    };
+    console.log(movieSelected);
 
     return (
         <section className="row">
@@ -42,6 +51,7 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
                                     : movie.backdrop_path
                             }`}
                             alt={movie.name}
+                            onClick={() => handleClick(movie)}
                         />
                     ))}
                 </div>
@@ -57,6 +67,9 @@ export default function Row({ title, id, fetchUrl, isLargeRow }) {
                     </span>
                 </div>
             </div>
+            {modalOpen && (
+                <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+            )}
         </section>
     );
 }
